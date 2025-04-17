a8f65482c8619066401f0eaba896cde16b23c32c
Maven-MJ <manoj@maventechno.com>
2025-04-17 20:12:37 +0530

    New Relic Demo app for crash

---
+++ DemoApp/DemoApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewRelic.Xamarin.Plugin;
+using Xamarin.Forms;
+
+namespace DemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                string value = null;
+                int length = value.Length;
+
+                //int.Parse("not a number");
+            }
+            catch (Exception ex)
+            {
+               // CrossNewRelicClient.Current.RecordException(ex);
+            }
+        }
+    }
+}
+
---
+++ DemoApp/DemoApp/NewRelicAnalyticsProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NewRelic.Xamarin.Plugin;
+using Xamarin.Essentials;
+
+namespace DemoApp
+{
+    public static class NewRelicAnalyticsProvider
+    {
+        public static void Start()
+        {
+            StartNewRelic();
+        }
+
+        private static void StartNewRelic()
+        {
+            CrossNewRelicClient.Current.HandleUncaughtException(false);
+            AgentStartConfiguration agentConfig = new AgentStartConfiguration();
+
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                CrossNewRelicClient.Current.Start("key", agentConfig);
+            }
+            else if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                CrossNewRelicClient.Current.Start("key", agentConfig);
+            }
+        }
+
+    }
+}
+
